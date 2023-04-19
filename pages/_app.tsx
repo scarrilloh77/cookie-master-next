5ec21643ae464f27aaadfee9102f0ffa81f024c5
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-04-19 01:31:04 -0500

    Establecer theme usando cookie - frontend

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,28 @@ import { CssBaseline, Theme, ThemeProvider } from '@mui/material';
 import { AppContext, AppProps } from 'next/app';
 import { customTheme, darkTheme, lightTheme } from '../themes';
 import '../styles/globals.css';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 interface Props extends AppProps {
   theme: string;
 }
 
 function MyApp({ Component, pageProps, theme = 'dark' }: Props) {
-  const currentTheme: Theme =
-    theme === 'light' ? lightTheme : theme === 'dark' ? darkTheme : customTheme;
+  const [currentTheme, setCurrentTheme] = useState(lightTheme);
+
+  // No se ejecuta en el server:
+  useEffect(() => {
+    const cookieTheme = Cookies.get('theme') || 'light';
+    const selectedTheme =
+      cookieTheme === 'light'
+        ? lightTheme
+        : cookieTheme === 'dark'
+        ? darkTheme
+        : customTheme;
+
+    setCurrentTheme(selectedTheme);
+  }, []);
 
   return (
     <ThemeProvider theme={currentTheme}>
